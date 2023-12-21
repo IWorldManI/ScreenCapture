676fa4da4ae2e7f42232d1972f311324865ed2db
Руденко Роман <rudenko@voltmart.su>
2023-12-21 17:39:35 +0300

    Add basic MVC structure Implement Model, Controller  class

---
+++ ScreenCaptureController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapture
+{
+    public class ScreenCaptureController
+    {
+        private ScreenCaptureModel model;
+        private MainForm view;
+
+        public ScreenCaptureController(ScreenCaptureModel model, MainForm view)
+        {
+            this.model = model;
+            this.view = view;
+
+            view.MouseDown += MainForm_MouseDown;
+            view.MouseMove += MainForm_MouseMove;
+            view.MouseUp += MainForm_MouseUp;
+            view.Paint += MainForm_Paint;
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            model.StartDrag(e.Location);
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                model.UpdateDrag(e.Location);
+                view.Invalidate();
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                OnDragComplete(e);
+            }
+        }
+
+        private void OnDragComplete(MouseEventArgs e)
+        {
+            Bitmap screenshot = model.GetScreenshot();
+            Clipboard.SetImage(screenshot);
+
+            model.ResetSelection();
+            view.Invalidate();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.White, 2))
+            {
+                e.Graphics.DrawRectangle(pen, model.SelectedRegion);
+            }
+        }
+    }
+}
---
+++ ScreenCaptureModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenCapture
+{
+    public class ScreenCaptureModel
+    {
+        public Rectangle SelectedRegion { get; private set; }
+
+        public void StartDrag(Point startPoint)
+        {
+            SelectedRegion = new Rectangle(startPoint.X, startPoint.Y, 0, 0);
+        }
+
+        public void UpdateDrag(Point currentPoint)
+        {
+            SelectedRegion = new Rectangle(SelectedRegion.Left, SelectedRegion.Top,
+                                          currentPoint.X - SelectedRegion.Left, currentPoint.Y - SelectedRegion.Top);
+        }
+
+        public Bitmap GetScreenshot()
+        {
+            Bitmap screenBmp = new Bitmap(SelectedRegion.Width, SelectedRegion.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics bmpGraphics = Graphics.FromImage(screenBmp))
+            {
+                bmpGraphics.CopyFromScreen(SelectedRegion.Left, SelectedRegion.Top, 0, 0, new Size(SelectedRegion.Width, SelectedRegion.Height));
+            }
+
+            return screenBmp;
+        }
+
+        public void ResetSelection()
+        {
+            SelectedRegion = Rectangle.Empty;
+        }
+    }
+}
